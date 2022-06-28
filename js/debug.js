56a4d343713b0839f89a5c135dd03c942c6ecf76
yamisenyuki <yami@git.tat.moe>
2022-06-29 00:02:48 +0800

    add debug tool page

---
+++ js/debug.js
@@ -0,0 +1,17 @@
+var main = new Vue({
+  el: "#main",
+  data: {
+    mqtt: {
+      host: "",
+      port: "",
+      endPoint: "",
+      loginName: "",
+      password: "",
+      clientid: "",
+      commandTopic: "",
+      statusTopic: "",
+    }
+
+  },
+  methods: {}
+})
